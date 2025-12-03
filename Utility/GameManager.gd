fff017cf4e11fdaf847b3777535515e64b99c511
Zaz <zazorah@gmail.com>
2025-12-03 10:15:55 -0700

    Defined a Theme for Textboxes

--- Utility/GameManager.gd
+++ Utility/GameManager.gd
@@ -6,12 +6,4 @@ extends Node
 var camera: Camera 
 
 func _ready() -> void:
-	var player = DialogManager.play_dialog("frog_test")
-	await player.finished
-	
-	print("First scene finished!")
-	
-	var new_player = DialogManager.play_dialog("frog_test")
-	await new_player.finished
-	
-	print("Second scene finished!")
+	var player = DialogManager.play_dialog("axolotl_test")
