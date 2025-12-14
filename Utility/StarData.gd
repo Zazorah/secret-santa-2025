b1f0f6b1f6b201a8335bd28167e868e59c4a0a2f
Zaz <zazorah@gmail.com>
2025-12-13 18:36:03 -0700

    Star Data, Star Prop, and Constellation Prop

--- Utility/StarData.gd
+++ Utility/StarData.gd
@@ -3,13 +3,32 @@ extends Resource
 
 ## Data object for tracking star count and rank.
 
-@export var star_rank = 3 # Ranges from 1 - 4
-@export var star_count = 0
-@export var constellation_count = 0
+var star_rank = 3 # Ranges from 1 - 4
+var star_count = 0
+var constellation_count = 0
+
+var _collected_stars: Array[StringName]
+var _constellation_points: Dictionary[StringName, StringName] = {
+	"PrefillPointKey": "PrefillStarKey"
+}
 
 const RANK_MAX = 4
 
 signal rank_increased(new_rank) 
+
+func set_constellation_point(constellation_key: StringName, star_key: StringName) -> void:
+	_constellation_points.set(constellation_key, star_key)
+	_collected_stars.push_back(star_key)
+
+func star_is_collected(star_key: StringName) -> bool:
+	return _collected_stars.has(star_key)
+
+func constellation_point_get_key(constellation_key: StringName) -> StringName:
+	return _constellation_points.get(constellation_key, "")
+
+func constellation_point_is_set(constellation_key: StringName) -> bool:
+	var constellation_point_filled = _constellation_points.get(constellation_key, "")
+	return constellation_point_filled != ""
 
 func increase_rank() -> void:
 	star_rank = min(RANK_MAX, star_rank + 1)
